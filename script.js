220314e0e524d24d50caf4c4e5a033b21b7a1f67
Jacob Tice <38109@ocvts.org>
2025-01-07 18:03:59 +0000

    Jacob JS functionality restored

--- script.js
+++ script.js
@@ -1,61 +1,51 @@
-let currentPlayer = 'x';
+let p1= 'X';
+let p2= 'O';
+let currentPlayer = p1;
 let winner = '';
 let turnCounter = 0;
 let classArray = ['col1', 'col2', 'col3', 'row1', 'row2', 'row3', 'dia1', 'dia2'];
 let P1Score = 0;
 let P2Score = 0;
-let caneArray = [];
-let wreathArray = [];
 
-cursor()
+// cursor()
 /********************* Function handles the player input and places mark on board. This may be completed as more than one function if you choose ********************************/
-function playerMove(imgNum) {
-if (winner == '') {
-   if (document.getElementById(imgNum).src == '' || document.getElementById(imgNum).src == 'https://cautious-yodel-5gxjqjp96p66c76jq-5501.app.github.dev/transparent.png'){
+function playerMove(boxNum) {
 
-   if (currentPlayer == 'x') {
-      document.getElementById(imgNum).src = "cane.png";
+if (winner == '') {
+   if (document.getElementById(boxNum).innerHTML == ''){
+
+   if (currentPlayer == p1) {
+      document.getElementById(boxNum).innerHTML = p1;
    } else {
-      document.getElementById(imgNum).src = "wreath.png"
+      document.getElementById(boxNum).innerHTML = p2;
    }
    
  checkWinner()
+//  alert(currentPlayer)
  turnCounter++
  if (turnCounter >= 9 && winner == '') {
    document.getElementById('winnerStatus').innerHTML = "It's a Tie!"
    setTimeout(resetGame, 4000)
    }
 
-   if (currentPlayer == 'x') {
-    currentPlayer = 'o';
+   if (currentPlayer == p1) {
+    currentPlayer = p2;
    } else {
-    currentPlayer = 'x';
+    currentPlayer = p1;
       }
-      cursor()
+    //   cursor()
    }
    }
 }
-
 /********************* Function checks all rows, columns, and diagonals for three identical symbols ********************************/
 function checkWinner() {
-for (let i =0; i<classArray.length; i++) {
-  const elements = document.getElementsByClassName(classArray[i]);
-  if (elements[0].src != '' && elements[0].src != 'https://cautious-yodel-5gxjqjp96p66c76jq-5501.app.github.dev/transparent.png') {
-  if (elements[0].src == elements[1].src && elements[0].src == elements[2].src){  
-   winner = currentPlayer;
-   
-   document.getElementById('winnerStatus').innerHTML = 'Player ' + winner.toUpperCase() + ' wins!'
-   
-   if (currentPlayer == 'x') {
-      P1Score++
-   } else {
-      P2Score++
-   }
-
-   document.getElementById('p1s').innerHTML = "P1's Score = " + P1Score
-   document.getElementById('p2s').innerHTML = "P2's Score = " + P2Score
-
-   setTimeout(resetGame, 4000)
+for (let i=0; i<classArray.length; i++) {
+    const elements = document.getElementsByClassName(classArray[i])
+    if (elements[0].innerHTML != '') {
+        if (elements[0].innerHTML == elements[1].innerHTML && elements[0].innerHTML == elements[2].innerHTML){  
+        winner=currentPlayer;
+        
+        document.getElementById('winnerStatus').innerHTML = `Player ${currentPlayer} Wins!`
   }
   }
    }
@@ -63,30 +53,19 @@ for (let i =0; i<classArray.length; i++) {
 
 /********************* Function resets the game board ********************************/
 function resetGame() {
-currentPlayer = 'x';
+currentPlayer = p1;
 winner = '';
 turnCounter = 0;
 for (let i = 1; i<10; i++){
-document.getElementById(i.toString()).setAttribute('src', 'transparent.png');
+document.getElementById(i).innerHTML = '';
 }
-cursor()
 }
 
-function cursor() {
+// function cursor() {
 
-   if(currentPlayer == 'x'){
-       document.getElementsByTagName("body")[0].style.cursor = "url('X cursor.png'), auto";
-   }else{
-       document.getElementsByTagName("body")[0].style.cursor = "url('O cursor.png'), auto";
-   }
-   }
-
-document.getElementById('b1').addEventListener('click', function() { playerMove('1')});
-document.getElementById('b2').addEventListener('click', function() { playerMove('2')});
-document.getElementById('b3').addEventListener('click', function() { playerMove('3')});
-document.getElementById('b4').addEventListener('click', function() { playerMove('4')});
-document.getElementById('b5').addEventListener('click', function() { playerMove('5')});
-document.getElementById('b6').addEventListener('click', function() { playerMove('6')});
-document.getElementById('b7').addEventListener('click', function() { playerMove('7')});
-document.getElementById('b8').addEventListener('click', function() { playerMove('8')});
-document.getElementById('b9').addEventListener('click', function() { playerMove('9')});
+//    if(currentPlayer == p1){
+//        document.getElementsByTagName("body")[0].style.cursor = "url('X cursor.png'), auto";
+//    }else{
+//        document.getElementsByTagName("body")[0].style.cursor = "url('O cursor.png'), auto";
+//    }
+//    }
