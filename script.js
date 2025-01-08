e80129b2c2191e3a683e546b526acf943304f668
loadingassests <37219@ocvts.org>
2025-01-08 17:30:33 +0000

    Working name and Characters

--- script.js
+++ script.js
@@ -1,46 +1,39 @@
-let p1= 'X';
-let p2= 'O';
-let currentPlayer = p1;
+let p1Char= 'X';
+let p2Char= 'O';
+let currentPlayer = p1Char;
 let winner = '';
 let turnCounter = 0;
 let classArray = ['col1', 'col2', 'col3', 'row1', 'row2', 'row3', 'dia1', 'dia2'];
 let P1Score = 0;
 let P2Score = 0;
-<<<<<<< HEAD
-let caneArray = [];
-let wreathArray = [];
-let p1Name = '';
-let p2Name = '';
-let p1Char = '';
-let p2Char = '';
-=======
->>>>>>> origin/HEAD
+let p1Name = 'Brendan';
+let p2Name = 'Jacob';
 
 // cursor()
 /********************* Function handles the player input and places mark on board. This may be completed as more than one function if you choose ********************************/
 function playerMove(boxNum) {
 
-if (winner == '') {
+if (winner == false) {
    if (document.getElementById(boxNum).innerHTML == ''){
 
-   if (currentPlayer == p1) {
-      document.getElementById(boxNum).innerHTML = p1;
+   if (currentPlayer == p1Char) {
+      document.getElementById(boxNum).innerHTML = p1Char;
    } else {
-      document.getElementById(boxNum).innerHTML = p2;
+      document.getElementById(boxNum).innerHTML = p2Char;
    }
    
  checkWinner()
 //  alert(currentPlayer)
  turnCounter++
- if (turnCounter >= 9 && winner == '') {
+ if (turnCounter >= 9 && winner == false) {
    document.getElementById('winnerStatus').innerHTML = "It's a Tie!"
    setTimeout(resetGame, 4000)
    }
 
-   if (currentPlayer == p1) {
-    currentPlayer = p2;
+   if (currentPlayer == p1Char) {
+    currentPlayer = p2Char;
    } else {
-    currentPlayer = p1;
+    currentPlayer = p1Char;
       }
    }
    }
@@ -51,11 +44,14 @@ for (let i=0; i<classArray.length; i++) {
     const elements = document.getElementsByClassName(classArray[i])
     if (elements[0].innerHTML != '') {
         if (elements[0].innerHTML == elements[1].innerHTML && elements[0].innerHTML == elements[2].innerHTML){  
-        winner=currentPlayer;
-        
-        document.getElementById('winnerStatus').innerHTML = `Player ${currentPlayer} Wins!`
+        winner=true;
+        if (turnCounter % 2 == 0) {
+         document.getElementById('winnerStatus').innerHTML = `${p1Name} Wins!`
+        } else {
+         document.getElementById('winnerStatus').innerHTML = `${p2Name} Wins!`
+        }
 
-        if (currentPlayer == p1) {
+        if (currentPlayer == p1Char) {
          P1Score++
          document.getElementById('p1s').innerHTML = `P1 Score = ${P1Score}`
         } else {
@@ -69,46 +65,25 @@ for (let i=0; i<classArray.length; i++) {
 
 /********************* Function resets the game board ********************************/
 function resetGame() {
-currentPlayer = p1;
 winner = '';
 document.getElementById('winnerStatus').innerHTML = 'Winner Declaration'
 turnCounter = 0;
+
 for (let i = 1; i<10; i++){
 document.getElementById(i).innerHTML = '';
 }
-<<<<<<< HEAD
-cursor()
 p1Char = document.getElementById('xCharacter').value
 p2Char = document.getElementById('oCharacter').value
-document.getElementById('head1').innerHTML= p1Char + p2Char
-=======
->>>>>>> origin/HEAD
+p1Name = document.getElementById('p1Name').value
+p2Name = document.getElementById('p2Name').value
+currentPlayer = p1Char;
 }
 
 // function cursor() {
 
-<<<<<<< HEAD
-   if(currentPlayer == 'x'){
-       document.getElementsByTagName("body")[0].style.cursor = "url('X cursor.png'), auto";
-   }else{
-       document.getElementsByTagName("body")[0].style.cursor = "url('O cursor.png'), auto";
-   }
-   }
-
-document.getElementById('b1').addEventListener('click', function() { playerMove('1')});
-document.getElementById('b2').addEventListener('click', function() { playerMove('2')});
-document.getElementById('b3').addEventListener('click', function() { playerMove('3')});
-document.getElementById('b4').addEventListener('click', function() { playerMove('4')});
-document.getElementById('b5').addEventListener('click', function() { playerMove('5')});
-document.getElementById('b6').addEventListener('click', function() { playerMove('6')});
-document.getElementById('b7').addEventListener('click', function() { playerMove('7')});
-document.getElementById('b8').addEventListener('click', function() { playerMove('8')});
-document.getElementById('b9').addEventListener('click', function() { playerMove('9')});
-=======
-//    if(currentPlayer == p1){
+//    if(currentPlayer == p1Char){
 //        document.getElementsByTagName("body")[0].style.cursor = "url('X cursor.png'), auto";
 //    }else{
 //        document.getElementsByTagName("body")[0].style.cursor = "url('O cursor.png'), auto";
 //    }
 //    }
->>>>>>> origin/HEAD
